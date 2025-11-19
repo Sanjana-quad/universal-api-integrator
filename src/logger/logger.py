3616f98860bc3842c1c2c0d52e23721222d74c6a
Sanjana <sanjanakallu2013@gmail.com>
2025-11-19 16:22:40 +0530

    color_formatter class for color coded error logs

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -26,6 +26,20 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 
+class ColorFormatter(logging.Formatter):
+    COLORS = {
+        "DEBUG": "\033[94m",   # Blue
+        "INFO": "\033[92m",    # Green
+        "WARNING": "\033[93m", # Yellow
+        "ERROR": "\033[91m",   # Red
+        "CRITICAL": "\033[95m" # Magenta
+    }
+    RESET = "\033[0m"
+    def format(self, record):
+            color = self.COLORS.get(record.levelname, self.RESET)
+            message = super().format(record)
+            return f"{color}{message}{self.RESET}"
+    
 def get_logger(name: str):
     # ensure logs directory exists
     os.makedirs("logs", exist_ok=True)
@@ -50,8 +64,11 @@ def get_logger(name: str):
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         )
+        color_formatter = ColorFormatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
         file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
+        stream_handler.setFormatter(color_formatter)
 
         logger.addHandler(file_handler)
         logger.addHandler(stream_handler)
